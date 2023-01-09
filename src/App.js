3ee8e83523e6623391ce4329d4cce293bdd1ba2d
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-01-10 00:16:19 +0530

    feat: init v3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import "./index.css";
+import { Typography } from "@mui/material";
+import { Theme } from "./theme/theme";
+
+
 function App() {
   return (
     <>
+      <Theme>
+        <Typography className="text-3xl font-bold underline" color="primary" variant="h3">Hello world!</Typography>
+      </Theme>
     </>
   );
 }
