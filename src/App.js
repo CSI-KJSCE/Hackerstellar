6f5b46d6b77abb5d4c74477a8c9d3906a9fd4c89
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-01-10 18:33:23 +0530

    bug: a few navbar bugs yet to be fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
+import React from "react";
 import "./index.css";
 import { Theme } from "./theme/theme";
 import TopNavbar from "./components/TopNavbar";
 
-
 function App() {
+  let current = "home";
+
+  const handleScroll = () => {
+    const sections = document.querySelectorAll(".section");
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    return current;
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
+  console.log(handleScroll());
+
   return (
     <>
       <Theme>
-        <TopNavbar />
+        <TopNavbar current={current} />
+        <section className="section" id="home">
+          <h1>Home</h1>
+        </section>
+        <section className="section" id="about">
+          <h1>About</h1>
+        </section>
+        <section className="section" id="domains">
+          <h1>Domains</h1>
+        </section>
+        <section className="section" id="prizes">
+          <h1>Prizes</h1>
+        </section>
+        <section className="section" id="timeline">
+          <h1>Timeline</h1>
+        </section>
+        <section className="section" id="sponsors">
+          <h1>Sponsors</h1>
+        </section>
+        <section className="section" id="faq">
+          <h1>FAQs</h1>
+        </section>
       </Theme>
     </>
   );
--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,26 +1,59 @@
-import React from 'react'
-import styles from './styles/TopNavbar.module.css'
+import React from "react";
+import "./styles/TopNavbar.css";
 
-const TopNavbar = () => {
+const TopNavbar = (props) => {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.navbar_logo__container}>
-        <img src={require('../data/assets/logo.svg').default} alt='' className={styles.navbar_logo}/>
+    <nav className="navbar">
+      <div className="navbar_logo__container">
+        <img
+          src={require("../data/assets/logo.svg").default}
+          alt=""
+          className="navbar_logo"
+        />
       </div>
-      <ul>
-        <li><a href='#home'>Home</a></li>
-        <li><a href='#about'>About</a></li>
-        <li><a href='#domains'>Domains</a></li>
-        <li><a href='#prizes'>Prizes</a></li>
-        <li><a href='#timeline'>Timeline</a></li>
-        <li><a href='#sponsors'>Sponsors</a></li>
-        <li><a href='#faq'>FAQs</a></li>
+      <ul className="menu">
+        <li className={`home ${props.current === "home" ? "active" : null}`}>
+          <a href="#home">Home</a>
+        </li>
+        <li className={`about ${props.current === "about" ? "active" : null}`}>
+          <a href="#about">About</a>
+        </li>
+        <li
+          className={`domains ${props.current === "domains" ? "active" : null}`}
+        >
+          <a href="#domains">Domains</a>
+        </li>
+        <li
+          className={`prizes ${props.current === "prizes" ? "active" : null}`}
+        >
+          <a href="#prizes">Prizes</a>
+        </li>
+        <li
+          className={`timeline ${
+            props.current === "timeline" ? "active" : null
+          }`}
+        >
+          <a href="#timeline">Timeline</a>
+        </li>
+        <li
+          className={`sponsors ${
+            props.current === "sponsors" ? "active" : null
+          }`}
+        >
+          <a href="#sponsors">Sponsors</a>
+        </li>
+        <li className={`faq ${props.current === "faq" ? "active" : null}`}>
+          <a href="#faq">FAQs</a>
+        </li>
       </ul>
-      <div className={styles.navbar_hamburger__container}>
-        <img src={require('../data/assets/hamburger.svg').default} alt='' className={styles.navbar_hamburger}/>
+      <div className="navbar_hamburger__container">
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" htmlFor="menu-btn">
+          <span className="navicon"></span>
+        </label>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default TopNavbar
+export default TopNavbar;
