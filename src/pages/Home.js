153032f84a04530c5c6a56f97fe86afcd83aaee1
The-Beyonder <kanishk.ks@somaiya.edu>
2023-01-13 15:20:32 +0530

    feat:Logo and Bg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,15 @@ const Home = () => {
   return (
     <>
       <div className="home">
-        <div class="main"></div>
+      <div className="background-container">
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+      </div>
+        <img
+          src='http://logo-hack.surge.sh/hackerstellar.png'
+          alt=""
+          className="home_logo"
+        ></img>
         <img
           src={require("../data/assets/astro.png")}
           alt=""
