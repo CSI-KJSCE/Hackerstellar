b4fdfb0e1ffb95422d3b749312aa7bb1f85794c4
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-01-16 00:26:30 +0530

    feat: faq page styles

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -19,16 +19,35 @@ const Faq = () => {
         <div className="faq-content">
           {data.map((content) => (
             <div className="faq-question" key={content.question}>
-              <Accordion >
+              <Accordion sx={{
+                width: "100%",
+                margin: "1rem auto",
+                backdropFilter: "blur(2px) saturate(200%)",
+                backgroundColor: "rgba(var(--foreground-color-rgb), 0.75)",
+                borderRadius: "1rem",
+                border: "1px solid rgba(var(--border-color-rgb), 0.2)",
+              }}>
                 <AccordionSummary
                   expandIcon={<ExpandMoreIcon />}
                   aria-controls="panel1a-content"
                   id="panel1a-header"
+                  sx={{
+                    color: "#cfcfcf",
+                    fontSize: "1.1rem",
+                  }}
                 >
-                  <Typography>{content.question}</Typography>
+                  <Typography  sx={{
+                    color: "#e56806",
+                    fontWeight: "bold",
+                    fontSize: "1.15rem",
+                    textTransform: "uppercase",
+                  }}>{content.question}</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                  <Typography>{content.answer}</Typography>
+                  <Typography sx={{
+                    color: "#cfcfcf",
+                    fontSize: "1.1rem",
+                  }}>{content.answer}</Typography>
                 </AccordionDetails>
               </Accordion>
             </div>
