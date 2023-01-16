6393e631e4f57c64a2134042198ea85960ac3de5
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-01-17 01:38:53 +0530

    feat: not built page added

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ import Background from "./components/Background";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/Faq";
+import Sponsors from "./pages/Sponsors";
 import Preloader from "./components/PreLoader";
+import NotBuilt from "./pages/NotBuilt";
 
 function App() {
   const [showPreloader, setShowPreloader] = useState(true);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const [current, setCurrent] = useState("home");
 
   const handleScroll = () => {
@@ -26,38 +29,47 @@ function App() {
   };
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", () => setScreenWidth(window.innerWidth));
   }, []);
 
-  return (
-    <>
-      <Preloader show={showPreloader}/>
-      <TopNavbar current={current} />
-      <LeftNavbar current={current} />
-      <RightNavbar />
-      <Background />
-      <section className="section" id="home">
-        <Home setShowPreloader={setShowPreloader}/>
+  if (screenWidth < 1200) {
+    return (
+      <section className="section" id="not-built">
+        <NotBuilt />
       </section>
-      <section className="section" id="about">
-        <About current={current} />
-      </section>
-      <section className="section" id="domains">
-        <h1>Domains</h1>
-      </section>
-      <section className="section" id="prizes">
-        <h1>Prizes</h1>
-      </section>
-      <section className="section" id="timeline">
-        <h1>Timeline</h1>
-      </section>
-      <section className="section" id="sponsors">
-        <h1>Sponsors</h1>
-      </section>
-      <section className="section" id="faq">
-        <Faq />
-      </section>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <Preloader show={showPreloader} />
+        <TopNavbar current={current} />
+        <LeftNavbar current={current} />
+        <RightNavbar />
+        <Background />
+        <section className="section" id="home">
+          <Home setShowPreloader={setShowPreloader} />
+        </section>
+        <section className="section" id="about">
+          <About current={current} />
+        </section>
+        <section className="section" id="domains">
+          <h1>Domains</h1>
+        </section>
+        <section className="section" id="prizes">
+          <h1>Prizes</h1>
+        </section>
+        <section className="section" id="timeline">
+          <h1>Timeline</h1>
+        </section>
+        <section className="section" id="sponsors">
+          <Sponsors />
+        </section>
+        <section className="section" id="faq">
+          <Faq />
+        </section>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/NotBuilt.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../index.css";
+import "./styles/NotBuilt.css";
+import { Theme } from "../data/assets/theme";
+import Background from "../components/Background";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
+function NotBuilt() {
+  return (
+    <>
+      <Background />
+      <Theme>
+        <Grid container justifyContent="center">
+          <Grid item mobile={10} tablet={8} laptop={6.5}>
+            <Box
+              sx={{
+                width: "100%",
+                height: "auto",
+                marginTop: "25vh",
+                p: 5,
+                py: 7,
+                transform: "translateY(-50%)",
+                overflow: "hidden",
+                backdropFilter: "blur(15px) saturate(200%)",
+                backgroundColor: "rgba(var(--foreground-color-rgb), 0.75)",
+                border: "1px solid rgba(var(--border-color-rgb), 0.2)",
+              }}
+            >
+              <h1 className="incomplete_title">Website Under Construction</h1>
+              <p className="incomplete_content">
+                We apologize for the inconvenience, our website is currently
+                under construction and for the best viewing experience, we
+                suggest using a laptop or desktop computer.
+              </p>
+            </Box>
+          </Grid>
+        </Grid>
+      </Theme>
+    </>
+  );
+}
+
+export default NotBuilt;
