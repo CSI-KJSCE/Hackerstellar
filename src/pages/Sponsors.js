58b0fd7e6d9c83960c1fd42f7d7a53558f42890d
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-01-19 14:09:43 +0530

    feat: contact us v1

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './styles/Sponsors.css';
+import Button from '../components/Button';
 
 const Sponsors = () => {
   return (
-    <div>Sponsors</div>
+    <>
+      <div className='sponsor'>
+        <div className='sponsor_title'>
+          <h1>Sponsors</h1>
+          <p> <span>&gt;</span> Want to become a Sponsor? Hit the button below! <span>&lt;</span> </p>
+        </div>
+        <div className='sponsor_button'>
+          <Button text="Contact Us"/>
+        </div>
+      </div>
+    </>
   )
 }
 
