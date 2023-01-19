9e4d8279e1baa4194b4f0b95a6dad03e77f409b3
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-01-19 14:25:55 +0530

    feat: sponsor page completed

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import './styles/Sponsors.css';
-import Button from '../components/Button';
+import React from "react";
+import "./styles/Sponsors.css";
 
 const Sponsors = () => {
   return (
     <>
-      <div className='sponsor'>
-        <div className='sponsor_title'>
+      <div className="sponsor">
+        <div className="sponsor_title">
           <h1>Sponsors</h1>
-          <p> <span>&gt;</span> Want to become a Sponsor? Hit the button below! <span>&lt;</span> </p>
+          <p>
+            {" "}
+            <span>&gt;</span> Want to become a Sponsor? Hit the button below!{" "}
+            <span>&lt;</span>{" "}
+          </p>
         </div>
-        <div className='sponsor_button'>
-          <Button text="Contact Us"/>
+        <div className="sponsor_button">
+          <div className="btn-wrap">
+            <div className="btn sponsor_btn-trapezoid-outline top">
+              <span>Sponsor Us</span>
+            </div>
+            <div className="sponsor_btn-trapezoid-outline2 bottom">
+              <span>Sponsor Us</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Sponsors
+export default Sponsors;
