10e0b46821bceca258ba3232482d37f8f9f07a1a
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-03-24 23:33:50 +0530

    feat: about page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,7 @@ const About = (props) => {
         <div className="about_heading">
           <h1>About</h1>
           <img
-            src="https://ik.imagekit.io/thebeyonder723/hackerstellar.png?ik-sdk-version=javascript-1.4.3&updatedAt=1673602246427"
+            src="/hackerstellar.webp"
             alt=""
             className="about_logo"
           ></img>
