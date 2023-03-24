01d4ee84b8ae837c76a10ed8be9e4075ba87a776
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-03-24 14:21:18 +0530

    refactor: basic code cleaning

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./styles/Footer.css";
-import mail from "../data/assets/mail.svg";
 const Footer = () => {
   return (
     <>
@@ -14,8 +13,8 @@ const Footer = () => {
             <h2>“We bid you Adieu” - Team CSI-KJSCE</h2>
           </div>
           <div className="footer-box-logo">
-            <img src={require("../data/assets/somaiya-two.png")} alt="mail" />
-            <img src={require("../data/assets/somaiya-one.png")} alt="mail" />
+            <img src="/somaiya-two.png" alt="mail" />
+            <img src="/somaiya-one.png" alt="mail" />
           </div>
         </div>
         <div className="footer-contact">
@@ -25,18 +24,12 @@ const Footer = () => {
               <a href="mailto:csi-kjsce@somaiya.edu">
                 <i className="fa-regular fa-envelope icon"></i>
               </a>
-            </li>
-            <li>
               <a href="https://www.instagram.com/csikjsce/?hl=en">
                 <i className="fa-brands fa-instagram icon"></i>
               </a>
-            </li>
-            <li>
               <a href="https://www.linkedin.com/company/csi---kjsce-student%27s-chapter/">
                 <i className="fa-brands fa-linkedin icon"></i>
               </a>
-            </li>
-            <li>
               <a href="https://www.youtube.com/@csikjsce3963">
                 <i className="fa-brands fa-youtube icon"></i>
               </a>
