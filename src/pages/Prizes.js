8736b6b7e46e65bf80b4db74d7d2860fd0ed7d0e
KCmean <kunalmanu2468@gmail.com>
2023-04-05 01:23:20 +0530

    feat: Prizes section

--- src/pages/Prizes.js
+++ src/pages/Prizes.js
@@ -1,20 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import './styles/Prizes.css';
+import React, { useState, useEffect } from "react";
+import "./styles/Prizes.css";
 
 const Prizes = () => {
   return (
     <>
-      <div className='prizes'>
-        <div className='prizes_title'>
+      <div className="prizes">
+        <div className="prizes_title">
           <h1>Prizes</h1>
-          <p> <span>&gt;</span> PRICES WILL BE DECLARED SOON  <span>&lt;</span> </p>
+          <p>
+            {" "}
+            <span>&gt;</span> PRICES ACCORDING TO SPECIFIC DOMAINS{" "}
+            <span>&lt;</span>{" "}
+          </p>
         </div>
-        <div className='prizes_incomplete'>
-          <h1>COMING  SOON</h1>
+        <div className="prizes_main_div">
+          <div className="prizes_galactic_genius">
+            <div className="prizes_gg_wrapper">
+              <div className="prizes_gg_title">GALACTIC GENIUS </div>
+              <div className="prizes_gg_content">CASH PRIZE, GOODIES, INTERNSHIPS AND MANY MORE !!! </div>
+            </div>
+            <div className="prizes_gg_subtitle">INR 15k+</div>
+          </div>
+          <div className="prizes_domains">
+            <div className="domain_child">
+              <div className="prizes_title_2">AI/ML</div>
+              <div className="prizes_subtitle">INR 10k+</div>
+              <div className="prizes_card_content">
+                CASH PRIZE AND GOODIES WORTH 1L+, INTERNSHIPS AND MANY MORE !!!
+              </div>
+            </div>
+            <div className="domain_child">
+              <div className="prizes_title_2">WEB/APP</div>
+              <div className="prizes_subtitle">INR 10k+</div>
+              <div className="prizes_card_content">
+                CASH PRIZE AND GOODIES WORTH 1L+, INTERNSHIPS AND MANY MORE !!!
+              </div>
+            </div>
+            <div className="domain_child">
+              <div className="prizes_title_2">BLOCKCHAIN</div>
+              <div className="prizes_subtitle">INR 10k+</div>
+              <div className="prizes_card_content">
+                CASH PRIZE AND GOODIES WORTH 1L+, INTERNSHIPS AND MANY MORE !!!
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Prizes;
