528ad61c5d36d15ec5271319e6fb51080ef06a6a
KCmean <kunalmanu2468@gmail.com>
2023-04-05 04:00:06 +0530

    feat: Media quries for prize component

--- src/pages/Prizes.js
+++ src/pages/Prizes.js
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from "react";
 import "./styles/Prizes.css";
+import data from "../data/prizes/prizes.json";
 
+const Prize = (props) => {
+  return (
+    <>
+      <div className="domain_child">
+        <div className="prizes_title_2">{props.domain}</div>
+        <div className="prizes_subtitle">INR {props.prize}k+</div>
+        <div className="prizes_card_content">{props.desc}</div>
+      </div>
+    </>
+  );
+};
 const Prizes = () => {
   return (
     <>
@@ -9,7 +21,7 @@ const Prizes = () => {
           <h1>Prizes</h1>
           <p>
             {" "}
-            <span>&gt;</span> PRICES ACCORDING TO SPECIFIC DOMAINS{" "}
+            <span>&gt;</span> PRIZES ACCORDING TO SPECIFIC DOMAINS{" "}
             <span>&lt;</span>{" "}
           </p>
         </div>
@@ -17,32 +29,23 @@ const Prizes = () => {
           <div className="prizes_galactic_genius">
             <div className="prizes_gg_wrapper">
               <div className="prizes_gg_title">GALACTIC GENIUS </div>
-              <div className="prizes_gg_content">CASH PRIZE, GOODIES, INTERNSHIPS AND MANY MORE !!! </div>
+              <div className="prizes_gg_content">
+                CASH PRIZE, GOODIES, INTERNSHIPS AND MANY MORE !!!{" "}
+              </div>
             </div>
             <div className="prizes_gg_subtitle">INR 15k+</div>
           </div>
           <div className="prizes_domains">
-            <div className="domain_child">
-              <div className="prizes_title_2">AI/ML</div>
-              <div className="prizes_subtitle">INR 10k+</div>
-              <div className="prizes_card_content">
-                CASH PRIZE AND GOODIES WORTH 1L+, INTERNSHIPS AND MANY MORE !!!
-              </div>
-            </div>
-            <div className="domain_child">
-              <div className="prizes_title_2">WEB/APP</div>
-              <div className="prizes_subtitle">INR 10k+</div>
-              <div className="prizes_card_content">
-                CASH PRIZE AND GOODIES WORTH 1L+, INTERNSHIPS AND MANY MORE !!!
-              </div>
-            </div>
-            <div className="domain_child">
-              <div className="prizes_title_2">BLOCKCHAIN</div>
-              <div className="prizes_subtitle">INR 10k+</div>
-              <div className="prizes_card_content">
-                CASH PRIZE AND GOODIES WORTH 1L+, INTERNSHIPS AND MANY MORE !!!
-              </div>
-            </div>
+            {data.map((item) => {
+              return (
+                <Prize
+                  key={item.title}
+                  domain={item.domain}
+                  prize={item.prize}
+                  desc={item.desc}
+                />
+              );
+            })}
           </div>
         </div>
       </div>
